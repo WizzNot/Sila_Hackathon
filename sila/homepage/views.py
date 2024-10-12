10bd347c80cf057e9f24c61191490e453a44f7a8
WizzNot <azaza22122004@gmail.com>
2024-10-12 18:52:02 +0500

    added images

--- sila/homepage/views.py
+++ sila/homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
+from PIL import Image
 import uuid
 import os
 
@@ -25,6 +26,7 @@ def upload(request):
     if request.method == 'POST' and request.FILES.getlist('images'):
         images = request.FILES.getlist('images')
         image_names = []
+        predicted_images = []
         for image in images:
             unique_filename = str(uuid.uuid4()) + os.path.splitext(image.name)[-1]
             fs = FileSystemStorage()
@@ -33,5 +35,10 @@ def upload(request):
         results = model.predict(os.path.join(settings.BASE_DIR, "images"))
         for i in image_names:
             delete_image(i)
-        return render(request, 'homepage/upload_success.html')
+        for i, r in enumerate(results):
+            name = image_names[i]
+            print(name)
+            r.save(filename=os.path.join("src", "predicted_images", name))
+            predicted_images.append(name)
+        return render(request, 'homepage/upload_success.html', {"images": predicted_images})
     return render(request, 'homepage/upload_fail.html')
