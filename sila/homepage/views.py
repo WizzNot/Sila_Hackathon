ef39a4658a5bb965406a00322415b2670290fe77
WizzNot <azaza22122004@gmail.com>
2024-10-12 15:55:05 +0500

    many images

--- sila/homepage/views.py
+++ sila/homepage/views.py
@@ -7,6 +7,9 @@ def homepage(request):
 
 
 def upload(request):
-    if request.method == 'POST' and request.FILES['image']:
+    if request.method == 'POST' and request.FILES.getlist('images'):
+        images = request.FILES.getlist('images')
+        for image in images:
+            print(image)
         return render(request, 'homepage/upload_success.html')
     return render(request, 'homepage/upload_fail.html')
